bb28de434f216985cbdb89be28631a30b74ae960
777Awesomesun <139230961+777Awesomesun@users.noreply.github.com>
2023-10-06 00:40:46 +0200

    Various changes...

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-import hang_list
 
-print(hang_list.seven_letter_words)
-print(len(hang_list.seven_letter_words))
-print(hang_list.seven_letter_words[3])
+lang = print(int(input("1 För Engelska ord eller 2 för Svenska:")))
+#import word list in users choice of language
+if lang == 1:
+    import word_list_eng as word_list
+else:
+    import word_list_swe as word_list
+
+
+
+import random
+
+challenge_level = int(input("Välj längd på ordet. 7, 8, 9, 10 eller 11:"))
+
+while True:
+    if challenge_level == 7:
+        hidden_word = random.choice(word_list.seven_letter_words)
+        break
+    elif challenge_level == 8:
+        hidden_word = random.choice(word_list.eight_letter_words)
+        break
+    elif challenge_level == 9:
+        hidden_word = random.choice(word_list.nine_letter_words)
+        break
+    elif challenge_level == 10:
+        hidden_word = random.choice(word_list.ten_letter_words)
+        break
+    elif challenge_level == 11:
+        hidden_word = random.choice(word_list.eleven_letter_words)
+        break
+    else:
+        print("\nFörsök igen.\n")
+        continue
+
+print(hidden_word)
---
+++ word_list_swe.py
@@ -0,0 +1,44 @@
+# 7-letter words
+seven_letter_words = [
+    "advokat",
+    "afrikan",
+    "bankbok",
+    "belåten",
+    "trevlig"
+]
+
+# 8-letter words
+eight_letter_words = [
+    "skrivare",
+    "allsidig",
+    "hälsning",
+    "aggregat",
+    "kreativa"
+]
+
+# 9-letter words
+nine_letter_words = [
+    "underhåll",
+    "gemenskap",
+    "förvandla",
+    "spännande",
+    "beräkning"
+]
+
+# 10-letter words
+ten_letter_words = [
+    "dagdrivare",
+    "bråkmakare",
+    "champinjon",
+    "upplevelse",
+    "utveckling"
+]
+
+# 11-letter words
+eleven_letter_words = [
+    "avställning",
+    "äventyrliga",
+    "askonsdagen",
+    "hälsosamhet",
+    "arkebuserar"
+]
