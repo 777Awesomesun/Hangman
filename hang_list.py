4cb1e226aec82a7cbc1fc6b96022884a8ec6eb31
777Awesomesun <139230961+777Awesomesun@users.noreply.github.com>
2023-10-05 20:43:31 +0200

    First push

---
+++ hang_list.py
@@ -0,0 +1,44 @@
+# 7-letter words
+seven_letter_words = [
+    "weather",
+    "bicycle",
+    "plumage",
+    "silence",
+    "forestry"
+]
+
+# 8-letter words
+eight_letter_words = [
+    "rainbow",
+    "elephant",
+    "favorite",
+    "necklace",
+    "mountain"
+]
+
+# 9-letter words
+nine_letter_words = [
+    "sunflower",
+    "scarecrow",
+    "butterfly",
+    "adventure",
+    "campfire"
+]
+
+# 10-letter words
+ten_letter_words = [
+    "harvesting",
+    "landscape",
+    "reflection",
+    "wildflower",
+    "meditation"
+]
+
+# 11-letter words
+eleven_letter_words = [
+    "celebration",
+    "environment",
+    "photography",
+    "remembrance",
+    "temperature"
+]
---
+++ main.py
@@ -0,0 +1,5 @@
+import hang_list
+
+print(hang_list.seven_letter_words)
+print(len(hang_list.seven_letter_words))
+print(hang_list.seven_letter_words[3])
